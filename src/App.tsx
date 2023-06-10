86779a9a69d66cfdfeaa75f59fd3c6e9e153500c
JXSR12 <90316325+JXSR12@users.noreply.github.com>
2023-06-10 14:43:00 +0700

    added side bar and slide in

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// App.tsx
+
 import React, { useState } from 'react';
 import './App.css';
 
@@ -5,54 +7,50 @@ type PlanetProps = {
   name: string;
   orbitalPeriod: number;
   index: number;
+  onClick: () => void;
 };
 
-const Planet: React.FC<PlanetProps> = ({ name, orbitalPeriod, index }) => {
-  const [showSidebar, setShowSidebar] = useState(false);
-
+const Planet: React.FC<PlanetProps> = ({ name, orbitalPeriod, index, onClick }) => {
   const animationDuration = `${orbitalPeriod}s`;
   const zIndexClassName = `zi-${20 - index}`;
   const planetClassName = `${name} planet`;
   const orbitSize = `${(index + 1) * 12}vmin`;
 
-  const handlePlanetClick = () => {
-    setShowSidebar(true);
-    console.log("CLICK")
-  };
-
   return (
     <div
       className={`${zIndexClassName} orbit`}
-      onClick={handlePlanetClick} 
+      onClick={onClick} 
       style={{
-
         width: orbitSize,
         height: orbitSize,
         animation: `spin ${animationDuration} linear infinite`
       }}
     >
       <div className="planet-holder">
-        <div className={planetClassName}/>
+        <div className={planetClassName} />
       </div>
-      {showSidebar && <Sidebar name={name} />}
     </div>
   );
 };
 
 type SidebarProps = {
   name: string;
+  onClose: () => void;
 };
 
-const Sidebar: React.FC<SidebarProps> = ({ name }) => {
+const Sidebar: React.FC<SidebarProps> = ({ name, onClose }) => {
   return (
-    <div className="sidebar">
+    <div className="sidebar slide-in">
       <h2>{name}</h2>
       {/* Additional planet information */}
+      <button onClick={onClose}>Close</button>
     </div>
   );
 };
 
 const App = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [selectedPlanet, setSelectedPlanet] = useState<string | null>(null);
   const earthOrbitalPeriodInSeconds = 10; // Earth year in seconds
   const planets = [
     { name: 'mercury', orbitalPeriod: (88 / 365.25) * earthOrbitalPeriodInSeconds },
@@ -65,15 +63,33 @@ const App = () => {
     { name: 'neptune', orbitalPeriod: (60182 / 365.25) * earthOrbitalPeriodInSeconds }
   ];
 
+  const handlePlanetClick = (planetName: string) => {
+    setSelectedPlanet(planetName);
+    setShowSidebar(true);
+    document.body.classList.add('sidebar-open');
+  };
+  
+  const handleCloseSidebar = () => {
+    setShowSidebar(false);
+    document.body.classList.remove('sidebar-open');
+  };  
+
   return (
     <div className="solar-system">
       <div className="sun" />
-      {planets.map((planet, index) => (
-        <Planet key={planet.name} name={planet.name} orbitalPeriod={planet.orbitalPeriod} index={index} />
-      ))}
       <video className="background-video" autoPlay loop muted>
         <source src="/assets/background.mp4" type="video/mp4" />
       </video>
+      {planets.map((planet, index) => (
+        <Planet 
+          key={planet.name} 
+          name={planet.name} 
+          orbitalPeriod={planet.orbitalPeriod} 
+          index={index} 
+          onClick={() => handlePlanetClick(planet.name)} 
+        />
+      ))}
+      {showSidebar && selectedPlanet && <Sidebar name={selectedPlanet} onClose={handleCloseSidebar} />}
     </div>
   );
 };
